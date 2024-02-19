d4dbb05cc52f61bb816f79b49e3538076b2a53dc
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-19 23:01:39 +0530

    freeapis.com

--- ___Handling__Api/freeapi_username.py
+++ ___Handling__Api/freeapi_username.py
@@ -3,7 +3,6 @@ def fetch_random_freeapi():
     url = "https://api.freeapi.app/api/v1/public/randomusers/user/random"
     response = requests.get(url)
     data = response.json()
-
     if data["success"] and "data" in data:
         user_data = data["data"]
         username = user_data["login"]["username"]
