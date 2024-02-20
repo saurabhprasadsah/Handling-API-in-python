c454ca54843d687067188aa89d067e29fd52ecc4
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-20 10:48:53 +0530

    freeapis.com

--- ___Handling__Api/Stocks_Api.py
+++ ___Handling__Api/Stocks_Api.py
@@ -1,3 +1,5 @@
+# Stocks Api
+
 import requests
 def Stcks_Api():
     url = "https://api.freeapi.app/api/v1/public/randomproducts?page=1&limit=10&inc=category%2Cprice%2Cthumbnail%2Cimages%2Ctitle%2Cid&query=mens-watches"
@@ -11,6 +13,7 @@ def Stcks_Api():
         return username, country
     else:
         raise Exception("failes to fetch user data")
+    
 
 
 def main():
