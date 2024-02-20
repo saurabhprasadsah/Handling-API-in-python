c077d278fe7b369ecd6fe465c2902f37fc68163a
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-20 11:46:25 +0530

    setinterval

--- ___Handling__Api/Javascript/closure.js
+++ ___Handling__Api/Javascript/closure.js
@@ -8,14 +8,29 @@
 
 // print()
 
-function count() {
-    var initial = 1;
-    function plus () {
-            console.log(++initial);
-    }
-   return plus; 
-}
+// function count() {
+//     var initial = 1;
+//     function plus () {
+//             console.log(++initial);
+//     }
+//    return plus; 
+// }
+// var c = count();
+// c();
+// c();
 
-var c = count();
-c();
+message = "Good global"
+function hello(){
+    let message = "Good morning";
+    {
+        let message ="Good afternoon";  //In this code let will be use in block scope not in the funciton scope.
+        console.log("Hello " + message);
+    }
+
+    let c  = function hello2(){
+        console.log(" i am  c "  + message); // A function with his lexical scoping in knows as the closure.
+    }
+    return c;
+}
+c = hello();
 c();
