356bbfe0dd143925520ab5f965996afa30e2478f
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-21 10:27:19 +0530

    uvicorn

--- __Fast__Api/main.py
+++ __Fast__Api/main.py
@@ -1,13 +1,17 @@
 from typing import Union
 
 from fastapi import FastAPI
-
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get("/rest")
+def saurabh():
+    return {"bunnu": "saurabh"}
 
 
 @app.get("/items/{item_id}")
