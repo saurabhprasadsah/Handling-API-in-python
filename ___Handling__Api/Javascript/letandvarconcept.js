e95e42dc7412594a48a460a5552e7a060f8e5937
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-22 11:09:39 +0530

    nodejsinterview

--- ___Handling__Api/Javascript/letandvarconcept.js
+++ ___Handling__Api/Javascript/letandvarconcept.js
@@ -33,4 +33,23 @@
 // console.log(a);  // 10
 
 
+// let obj1 = {a:10 , b:20}
+// let obj2 = Object.create(obj1)
+
+// obj2.k = 99
+
+// console.log(obj2);  // {} emypty object //{ k: 99 }
+// console.log(obj2.a); 
+
+
+// obj2.a++
+// obj1.a=44
+// console.log(obj2.a);  // 44  //copy for data 
+
+
+
+
+
+
+
 
