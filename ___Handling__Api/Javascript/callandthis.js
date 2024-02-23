36a0eb0651d857fa550d88b9e2bc97fbdbb9651c
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-23 23:09:25 +0530

    settimeout

--- ___Handling__Api/Javascript/callandthis.js
+++ ___Handling__Api/Javascript/callandthis.js
@@ -1,4 +1,23 @@
 /* call and this keywords use in javascript */
 
+// Call keyword: It is used to invoke a function. The call() method allows you to call
 
+// This keyword:
 
+function Setusername(username){
+    // complex db calls
+    this.username = username
+    console.log("called");
+}
+
+function createUser(username,email,password){
+    Setusername.call(this, username)
+    //console.log(`Username : ${this.username}`);
+    // this.username = username
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai","chai@fb.com", "123")
+console.log(chai);
+// createUser();
