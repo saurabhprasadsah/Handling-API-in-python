e7f7c4e62145d917cc2d3ebc1e56081394eca874
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-23 23:35:17 +0530

    class

---
+++ ___Handling__Api/Javascript/classandconstructor.js
@@ -0,0 +1,33 @@
+/* class and constructor */
+/* ======================= */
+// ES6
+
+// class User{
+//     constructor(username,email,password){
+//         this.username = username;
+//         this.email = email;
+//         this.password = password;
+//     }
+
+//     encryptpassword(){
+//         return `${this.password}abc`;
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+
+// }
+
+// const chai = new User("chai","chai@gmail.com", "123330")
+
+// console.log(chai);
+
+// console.log(chai.encryptpassword());
+// console.log(chai.changeUsername());
+
+
+
+
+
+
+
