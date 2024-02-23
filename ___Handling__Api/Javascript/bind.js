a6aae9f279a8a153fd9e9505d3aa3ad1b05e0b84
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-23 09:50:07 +0530

    javascriptquestion

--- ___Handling__Api/Javascript/bind.js
+++ ___Handling__Api/Javascript/bind.js
@@ -13,16 +13,28 @@
 //   console.log(boundGetX());
 //   // Expected output: 42
 
-function getThis() {
-    return this;
+// function getThis() {
+//     return this;
+//   }
+  
+//   const obj1 = { name: "obj1" };
+//   const obj2 = { name: "obj2" };
+  
+//   obj1.getThis = getThis;
+//   obj2.getThis = getThis;
+  
+//   console.log(obj1.getThis()); // { name: 'obj1', getThis: [Function: getThis] }
+//   console.log(obj2.getThis()); // { name: 'obj2', getThis: [Function: getThis] }
+
+
+function init() {
+    var name = "Mozilla"; // name is a local variable created by init
+    function displayName() {
+                         // displayName() is the inner function, that forms the closure
+      console.log(name); // use variable declared in the parent function
+    }
+    displayName();
   }
+  init();
   
-  const obj1 = { name: "obj1" };
-  const obj2 = { name: "obj2" };
-  
-  obj1.getThis = getThis;
-  obj2.getThis = getThis;
-  
-  console.log(obj1.getThis()); // { name: 'obj1', getThis: [Function: getThis] }
-  console.log(obj2.getThis()); // { name: 'obj2', getThis: [Function: getThis] }
   
