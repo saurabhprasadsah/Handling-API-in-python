1995de4d093d260acb2cadab52f1c4ad4b547b5e
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-25 11:43:00 +0530

    bindjs

--- ___Handling__Api/Javascript/fetchApi.js
+++ ___Handling__Api/Javascript/fetchApi.js
@@ -1,10 +1,12 @@
 fetch('https://api.github.com/users/saurabhprasadsah')
+
 .then((response) =>{
     return response.json()
 })
 .then((data) =>{
     console.log(data);
 })
+
 .catch((error) => console.log(error))
 
 
