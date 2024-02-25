ef454bd74265e96258d2baae2b6abc89acdda3e4
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-25 11:54:55 +0530

    bindjs

--- ___Handling__Api/Javascript/Async.js
+++ ___Handling__Api/Javascript/Async.js
@@ -1,18 +1,21 @@
 /* set timeout and setInterval */
 
+/*
 
-// function sayhello(){
-//     console.log("Hello  ");
+function sayhello(){
+    console.log("Hello  ");
     
-// }
+}
 
-// console.log("And the wait will be start here" );
+console.log("And the wait will be start here" );
 
-// var intervalid =setInterval(sayhello,1000) // continious interval time of setInterval will be running in this code.
+var intervalid =setInterval(sayhello,1000) // continious interval time of setInterval will be running in this code.
 
-// clearInterval(intervalid) // exit the setinterval the intervalId
+clearInterval(intervalid) // exit the setinterval the intervalId
 
-// setTimeout(sayhello, 5000)
-// setTimeout(sayhello, 3000)
+setTimeout(sayhello, 5000)
+setTimeout(sayhello, 3000)
+
+*/
 
 
