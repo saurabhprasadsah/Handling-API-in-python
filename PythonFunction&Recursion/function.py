07dc0cd07683fa121425caa147dde0c9a71cdcf9
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-25 20:22:44 +0530

    function

--- PythonFunction&Recursion/function.py
+++ PythonFunction&Recursion/function.py
@@ -1,10 +1,60 @@
-#Learn function in python 
+#Learn function in python function means the redundency  code can be removed by using a function.
+# function defination 
+# def sum(a,b): #parameter
+#     sum = a+b
+#     return sum
 
-def sum(a,b):
-    sum = a+b
-    return sum
+# print(sum(2,4)) #function call:argument
+# print(sum(4,4))
+# print(sum(10,4))
 
 
-print(sum(2,4))
-print(sum(4,4))
-print(sum(10,4))
+# #function defination with no parameter
+
+# def print_hello():
+#     print("Hello")
+
+# print_hello()
+# print_hello()
+# print_hello()
+
+# output =(print_hello())
+# print(output)
+
+
+# def avg(a,b,c):
+#     sum = a+b+c
+#     avg = sum/3
+#     return avg
+
+# print(avg(10,20,30))
+# print("apnacollege", end=" and ", sep="/n")
+# print("university" , sep="  ")
+# print("shardhasurbah")
+
+# n = 5
+# fact = 1
+
+# for i in range(1,n+1):
+#     fact = fact*i 
+# print(fact)    
+
+# n = 5
+# def factorial(n):
+#     fact = 1
+#     for i in range(1,n+1):
+#         fact = fact*i
+#     print(fact) 
+
+# factorial(40)    
+
+
+
+
+
+
+
+
+
+
+
