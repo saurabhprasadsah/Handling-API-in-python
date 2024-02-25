1a8008736fe15f178ea3f46f2426abb6c4e0c9f4
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-25 16:37:16 +0530

    xhrequest

--- ___Handling__Api/Javascript/fetchApi.js
+++ ___Handling__Api/Javascript/fetchApi.js
@@ -1,12 +1,10 @@
 fetch('https://api.github.com/users/saurabhprasadsah')
-
 .then((response) =>{
     return response.json()
 })
 .then((data) =>{
     console.log(data);
 })
-
 .catch((error) => console.log(error))
 
 
