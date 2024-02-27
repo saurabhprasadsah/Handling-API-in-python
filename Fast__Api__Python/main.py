d41e80f492ac7ae274227384d0d09422d5e05488
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-27 14:57:14 +0530

    fastapi

--- Fast__Api__Python/main.py
+++ Fast__Api__Python/main.py
@@ -3,8 +3,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-
 @app.get("/")
 def read_root():
     return {"Hello": "Saurabh how are you?"}
--- __Fast__Api/main.py
+++ __Fast__Api/main.py
@@ -1,41 +1,41 @@
-from typing import Union
-from fastapi import FastAPI, Body
-from fastapi import FastAPI
-app = FastAPI()
+# from typing import Union
+# from fastapi import FastAPI, Body
+# from fastapi import FastAPI
+# app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/rest")
-def saurabh():
-    return {"bunnu": "saurabh"}
+# @app.get("/rest")
+# def saurabh():
+#     return {"bunnu": "saurabh"}
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
-
-# @app.post("/items/")
-# async def create_item(item: dict):
-#     # Process the data in the request body
-#     return {"message": f"Item created successfully with data: {item}"}
-
-
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item = Body(...)):
-    # Update the item in your database or system
-    # based on item_id and item data
-    # ...
-
-    return {"message": "Item updated successfully"}
+# # @app.get("/items/{item_id}")
+# # def read_item(item_id: int, q: Union[str, None] = None):
+# #     return {"item_id": item_id, "q": q}
 
 
 
+# # @app.post("/items/")
+# # async def create_item(item: dict):
+# #     # Process the data in the request body
+# #     return {"message": f"Item created successfully with data: {item}"}
 
-import uvicorn
-uvicorn.run(app)
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item = Body(...)):
+#     # Update the item in your database or system
+#     # based on item_id and item data
+#     # ...
+
+#     return {"message": "Item updated successfully"}
+
+
+
+
+# import uvicorn
+# uvicorn.run(app)
