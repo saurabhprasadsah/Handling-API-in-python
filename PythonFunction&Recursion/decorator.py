202e485eec3cd35525b1b655b2b94bca8ace893a
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-27 12:43:14 +0530

    decoratorinoython

--- PythonFunction&Recursion/decorator.py
+++ PythonFunction&Recursion/decorator.py
@@ -1,19 +1,40 @@
-# print("Decorator function in python")
+# # print("Decorator function in python")
 
-import time
+# import time
+# def timer(func):
+#     def wrapper(*args,  **kawargs):
+#        start = time.time()
+#        result = func(*args,  **kawargs)
+#        end = time.time()
+#        print(f"{func.__name__} ran in {end-start} time")
+#        return result
+#     return wrapper
 
-def timer(func):
-    def wrapper(*args,  **kawargs):
-       start = time.time()
-       result = func(*args,  **kawargs)
-       end = time.time()
-       print(f"{func.__name__} ran in {end-start} time")
-       return result
-    return wrapper
+# @timer
+# def example_function(n):
+#     time.sleep(n)
+
+# example_function(5)
 
 
-@timer
-def example_function(n):
-    time.sleep(n)
 
-example_function(5)
+
+
+
+
+
+
+def greet(name, greeting="Hello"):
+    print(f"{greeting} {name}")
+
+greet("chai or Coffee", greeting="HanjiLekrAuo ")
+
+
+
+
+
+
+
+
+
+
