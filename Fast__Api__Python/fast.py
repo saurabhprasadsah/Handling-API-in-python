95dc3726713e05adb53ba1022b92d7ddb9663f1b
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-27 14:31:00 +0530

    fastapi

--- Fast__Api__Python/fast.py
+++ Fast__Api__Python/fast.py
@@ -1,8 +1,8 @@
 from typing import Union
-
 from fastapi import FastAPI
 
 app = FastAPI()
+
 
 
 @app.get("/")
@@ -10,14 +10,10 @@ def read_root():
     return {"Hello": "Saurabh how are you?"}
 
 
-
-
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-
 import uvicorn
-uvicorn.run(app)
+uvicorn.run(app)    
